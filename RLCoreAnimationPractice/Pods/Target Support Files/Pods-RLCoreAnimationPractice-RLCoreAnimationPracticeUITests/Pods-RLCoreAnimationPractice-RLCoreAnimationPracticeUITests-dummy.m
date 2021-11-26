ccf502e1904e806862bf74f4e0ef6eb19aa2467a
Riven <longruiwen@honbow.com>
2021-11-26 17:37:43 +0800

    创建练习工程

---
+++ RLCoreAnimationPractice/Pods/Target Support Files/Pods-RLCoreAnimationPractice-RLCoreAnimationPracticeUITests/Pods-RLCoreAnimationPractice-RLCoreAnimationPracticeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RLCoreAnimationPractice_RLCoreAnimationPracticeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RLCoreAnimationPractice_RLCoreAnimationPracticeUITests
+@end
---
+++ RLCoreAnimationPractice/Pods/Target Support Files/Pods-RLCoreAnimationPracticeTests/Pods-RLCoreAnimationPracticeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RLCoreAnimationPracticeTests : NSObject
+@end
+@implementation PodsDummy_Pods_RLCoreAnimationPracticeTests
+@end
